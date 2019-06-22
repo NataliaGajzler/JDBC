8ac0c3d9f4b72db1718ba62fd290827af19fb7f1
NataliaGajzler <NataliaGajzler@users.noreply.github.com>
2019-06-22 13:34:41 +0200

    prepareStatement2 + null protection wasNull

--- src/main/java/pl/sda/jdbcjpa/JdbcMain.java
+++ src/main/java/pl/sda/jdbcjpa/JdbcMain.java
@@ -5,9 +5,36 @@ import java.sql.*;
 
 public class JdbcMain {
     public static void main(String[] args) {
-        statement();
-        prepareStatement();
-        callableStatement();
+        //statement();
+       // prepareStatement();
+        //callableStatement();
+        prepareStatement2();
+
+    }
+
+    private static void prepareStatement2() {
+        try (Connection connection = getConnection()) {
+            int minSalary = 1000;
+            String query = "select ename, job, sal, mgr " +
+                    "from sdajdbc.employee " +
+                    "where sal > ?";
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setInt(1, minSalary);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                String ename = resultSet.getString("ename");
+                String job = resultSet.getString("job");
+                int sal = resultSet.getInt("sal");
+                Integer mgr = resultSet.getInt("mgr");
+                if (resultSet.wasNull()) { // sprawdza ostatnią wartość
+                    mgr = null;
+                }
+                System.out.println(ename + " " + job + " " + sal + " " + mgr);
+            }
+            System.out.println();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
     }
 
@@ -19,8 +46,8 @@ public class JdbcMain {
             callableStatement.registerOutParameter(2,Types.VARCHAR);
 
             callableStatement.execute();
-            String string = callableStatement.getString(2);
-            System.out.println(string);
+            String name = callableStatement.getString(2);
+            System.out.println(name);
         } catch (SQLException e) {
             e.printStackTrace();
         }
